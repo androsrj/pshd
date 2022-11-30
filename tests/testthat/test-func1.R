d356336b280f04db239589f84d9f1845abd8cd6e
Jacob Andros <androsrj@gmail.com>
2022-11-30 14:34:19 -0600

    made capitalization consistent in test names

--- tests/testthat/test-func1.R
+++ tests/testthat/test-func1.R
@@ -6,7 +6,7 @@ graph2 <- matrix(c(0, 0, 1,
                    1, 0, 1,
                    0, 1, 0), nrow = 3, byrow = TRUE)
 
-test_that("PSHD Loss is Computed Correctly, 3 Dimensions", {
+test_that("PSHD loss is computed correctly, 3 dimensions", {
   expect_equal(pshd(graph1, graph2, a = 1), 3)
   expect_equal(pshd(graph1, graph2, a = 0.5), 3.5)
   expect_equal(pshd(graph1, graph2, a = 1.9), 2.1)
@@ -24,7 +24,7 @@ graph4 <- matrix(c(0, 0, 1, 1, 1,
                    1, 1, 0, 0, 1,
                    0, 0, 1, 1, 0), nrow = 5, byrow = TRUE)
 
-test_that("PSHD Loss is Computed Correctly, 5 Dimensions", {
+test_that("PSHD loss is computed correctly, 5 dimensions", {
   expect_equal(pshd(graph3, graph4, a = 1), 7)
   expect_equal(pshd(graph3, graph4, a = 0.25), 7.75)
   expect_equal(pshd(graph3, graph4, a = 1.6), 6.4)
@@ -32,7 +32,7 @@ test_that("PSHD Loss is Computed Correctly, 5 Dimensions", {
 
 # Mismatching dimensions (3 and 5)
 # Warnings are to be expected here
-test_that("PSHD Loss is Computed Correctly, Mismatching Dimensions", {
+test_that("PSHD loss is computed correctly, mismatching dimensions", {
   expect_equal(pshd(graph1, graph4, a = 1), 11)
   expect_equal(pshd(graph1, graph4, a = 0.1), 19.1)
   expect_equal(pshd(graph1, graph4, a = 1.1), 10.1)
