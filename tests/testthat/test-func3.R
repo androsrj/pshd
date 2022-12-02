0b929fa0247efee457611f4ae1c172c1e7d8b1e8
Jacob Andros <androsrj@gmail.com>
2022-12-02 11:38:44 -0600

    added more tests for the estimate_pshd function to increase coverage

--- tests/testthat/test-func3.R
+++ tests/testthat/test-func3.R
@@ -12,6 +12,18 @@ samples[[4]][3, 1] <- 1
 # Run tests
 test_that("Find estimate with minimal PSHD loss, 3 nodes with 4 samples in list form", {
   expect_equal(estimate_pshd(samples, a = 1)$estimate, mainGraph)
-  expect_equal(estimate_pshd(samples, a = 0.5)$estimate, mainGraph)
+  expect_equal(estimate_pshd(samples, a = 1.01)$estimate, mainGraph)
+})
+
+# Set up sample graphs used for computing exp loss (3D array form)
+samples <- array(dim = c(3, 3, 4))
+samples[ , , 1] <- samples[ , , 2] <- samples[ , , 3] <- samples[ , , 4] <- mainGraph
+samples[1, 3, 1] <- 1
+samples[2, 1, 3] <- 0
+samples[3, 1, 4] <- 1
+
+# Run tests
+test_that("Find estimate with minimal PSHD loss, 3 nodes with 4 samples in 3D array form", {
+  expect_equal(estimate_pshd(samples, a = 1)$estimate, mainGraph)
   expect_equal(estimate_pshd(samples, a = 1.01)$estimate, mainGraph)
 })
